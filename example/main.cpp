9e80920b63e1e02a06f4e83ff73f1236ca409cc8
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2016-11-06 22:07:10 +0300

    Create main.cpp

---
+++ example/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "two-dimensional array.hpp"
+#include "stdafx.h"
+#include <tchar.h>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int **p;
+	int n = 4;
+	int m = 4;
+	int i, j;
+        
+    
+       
+    setlocale(LC_ALL,"rus");
+   
+    
+    cout<<"\nдвухмерный массив: " << p[i][j];
+
+ system("pause");
+}
